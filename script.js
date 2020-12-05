af3a51483040a8eb2a29b36bb0a36f35037faa4b
avpizarro <avpizarro@live.com>
2020-12-06 08:40:01 +1100

    Add second question

--- script.js
+++ script.js
@@ -25,18 +25,18 @@ startBtn.addEventListener("click", function(){
     console.log(timer);
     console.log(timeLeft);
 
-    // Ask First set of question
+    // Ask first question
     question.innerHTML = "Commonly used data types DO NOT include:";
     question.style.textAlign = "left";
 
-    // Add the answer options  
-
+    // Add the first question answer options  
     var answers = ["strings", "booleans", "alerts", "numbers"];
     answerButtons.innerHTML = "";
 
+    //Add a button for each answer option
     for (i = 0; i< answers.length; i++) {
         var btn = document.createElement("button");
-        btn.textContent = answers[i];
+        btn.textContent = ([i+1]+ ". " + answers[i]);
         answerButtons.appendChild(btn);
         btn.setAttribute("style", "float: left; margin-right: 90%; margin-top: 5px; margin-bottom: 5px; text-align: left;");
         btn.classList.add(answers[i]);
@@ -44,25 +44,50 @@ startBtn.addEventListener("click", function(){
         //Make the answer buttons clickable
         btn.addEventListener("click",function(event){
             var element = event.target;
-            var correctAnswer = document.querySelector(".alerts");
+            var correctAnswer1 = document.querySelector(".alerts");
 
-            if (element === correctAnswer) {
-                var response = document.createElement("div");
+
+                // Ask second question
+                question.innerHTML = "The condition in an if / else statement is enclosed within ____.";
+                question.style.textAlign = "left";
+
+                // Add the second question answer options  
+                var answers = ["quotes", "curly brackets", "parentheses", "square brackets"];
+                answerButtons.innerHTML = "";
+
+                //Turn the second answer options into buttons
+                for (i = 0; i< answers.length; i++) {
+                    var btn = document.createElement("button");
+                    btn.textContent = ([i+1]+ ". " + answers[i]);
+                    answerButtons.appendChild(btn);
+                    btn.setAttribute("style", "float: left; margin-right: 90%; margin-top: 5px; margin-bottom: 5px; text-align: left; width: 180px;");
+                    btn.classList.add(answers[i].replace(/\s/g, ''));
+                    console.log(btn);
+
+                }
+
                 var line = document.createElement("hr");
-                response.textContent = ("Correct!");
                 answerButtons.appendChild(line);
-                answerButtons.appendChild(response);
-                response.setAttribute("style", "text-align: left; font-style: italic; color: rgb(151, 149, 149);");
-            } 
-            
-            else {
                 var response = document.createElement("div");
-                var line = document.createElement("hr");
-                response.textContent = ("Wrong!");
-                answerButtons.appendChild(line);
-                answerButtons.appendChild(response);
                 response.setAttribute("style", "text-align: left; font-style: italic; color: rgb(151, 149, 149);");
-            }
+                
+
+                if (element === correctAnswer1) {
+                    response.textContent = ("Correct!");
+                    answerButtons.appendChild(response);
+                }
+
+                else {
+                    response.textContent = ("Wrong!");
+                    answerButtons.appendChild(response);
+                }
+
+                //Make the answer buttons clickable
+                btn.addEventListener("click",function(event){
+                    var element = event.target;
+                    var correctAnswer2 = document.querySelector(".parentheses");
+                }) 
+                    
 
         });    
     }
