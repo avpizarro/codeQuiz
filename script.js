afd418ff84ce5508c144e55ab0c6db7e3103654d
avpizarro <avpizarro@live.com>
2020-12-05 21:42:40 +1100

    Add first question and answers

--- script.js
+++ script.js
@@ -7,63 +7,67 @@ console.log(answerButtons);
 // Make the start button clickable
 startBtn.addEventListener("click", function(){
 
- // Set the timer   
+    // Set the timer   
     var timeLeft = 75;
+
     var quizTimer = setInterval(function(){
 
         timeLeft--;
         timer.textContent = ("Time: " + timeLeft);
 
-// Stop the timer
+        // Stop the timer
         if(timeLeft === 0)
-            clearInterval(quizTimer)
-        },1000); 
-        console.log(timer);
-        console.log(timeLeft);
+        clearInterval(quizTimer)
 
-// Ask First set of question
+    },1000); 
+
+
+    console.log(timer);
+    console.log(timeLeft);
+
+    // Ask First set of question
     question.innerHTML = "Commonly used data types DO NOT include:";
     question.style.textAlign = "left";
 
-// Add the answer options  
+    // Add the answer options  
 
     var answers = ["strings", "booleans", "alerts", "numbers"];
     answerButtons.innerHTML = "";
 
-for (i = 0; i< answers.length; i++) {
-    var btn = document.createElement("button");
-    btn.textContent = answers[i];
-    answerButtons.appendChild(btn);
-    btn.setAttribute("style", "float: left; margin-right: 90%; margin-top: 10px; text-align: left;");
-   
+    for (i = 0; i< answers.length; i++) {
+        var btn = document.createElement("button");
+        btn.textContent = answers[i];
+        answerButtons.appendChild(btn);
+        btn.setAttribute("style", "float: left; margin-right: 90%; margin-top: 5px; margin-bottom: 5px; text-align: left;");
+        btn.classList.add(answers[i]);
+    
+        //Make the answer buttons clickable
+        btn.addEventListener("click",function(event){
+            var element = event.target;
+            var correctAnswer = document.querySelector(".alerts");
 
-}
+            if (element === correctAnswer) {
+                var response = document.createElement("div");
+                var line = document.createElement("hr");
+                response.textContent = ("Correct!");
+                answerButtons.appendChild(line);
+                answerButtons.appendChild(response);
+                response.setAttribute("style", "text-align: left; font-style: italic; color: rgb(151, 149, 149);");
+            } 
+            
+            else {
+                var response = document.createElement("div");
+                var line = document.createElement("hr");
+                response.textContent = ("Wrong!");
+                answerButtons.appendChild(line);
+                answerButtons.appendChild(response);
+                response.setAttribute("style", "text-align: left; font-style: italic; color: rgb(151, 149, 149);");
+            }
+
+        });    
+    }
+
+});
 
 
-    /*var answersList = document.createElement("ul");
-    var btn1row = document.createElement("li"); 
-    var btn2row = document.createElement("li"); 
-    var btn3row = document.createElement("li"); 
-    var btn4row = document.createElement("li"); 
 
-    var btn1 = document.createElement("button");
-        btn1.textContent = "strings";
-
-    var btn2 = document.createElement("button");
-        btn2.textContent = "booleans";
-
-    var btn3 = document.createElement("button");
-        btn3.textContent = "alerts";
-
-    var btn4 = document.createElement("button");
-        btn4.textContent = "numbers";
-
-    answers.appendChild(answersList);
-    answersList.appendChildren(btn1row, btn2row, btn3row, btn4row);
-    btn1row.appendChild(btn1);
-    btn2row.appendChild(btn2);
-    btn3row.appendChild(btn3);
-    btn4row.appendChild(btn4);*/
-
-
-})
